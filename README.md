6cb740bfc91a511affe6df3a938168d5e8d2ba5e
amarkes <tmowna@gmail.com>
2017-03-28 08:41:29 -0300

    primeira versão

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-# brmasker-ionic
-mascara input para ionic
+# masker
+return mask in input
+
+# install
+
+```sh
+npm install brmasker
+```
+
+# HTML
+
+```html
+<input type="text" name="teste" [brmasker]="{mask:'00.000-000', len:10}">
+```
+
+# Module
+
+```javascript
+import { AppMaskerModule } from 'brmasker';
+@NgModule({
+  imports: [
+    AppMaskerModule
+  ],
+})
+
+```
---
+++ src/app/components/masker/masker.component.ts
@@ -0,0 +1,49 @@
+import { Component, Input, Output, Directive, forwardRef, Renderer, ElementRef} from '@angular/core';
+import { NG_VALUE_ACCESSOR, ControlValueAccessor, NgControl } from '@angular/forms'
+
+@Directive({
+  host: {
+    '(input)': 'onInput($event.target.value)',
+    //'(blur)': '_onTouched()'
+  },
+  selector: '[brmasker]',
+  providers: [{
+    provide: NG_VALUE_ACCESSOR,
+    useExisting: forwardRef(() => MaskerDirective),
+    multi: true
+  }]
+})
+export class MaskerDirective {
+  constructor(private renderer: Renderer, private element: ElementRef) {}
+  @Input('brmasker') brmaskere: any;
+  private onInput(value): void {
+    let ret = this.formataCampo(value, this.brmaskere.mask, this.brmaskere.len);
+    if (ret) {
+      this.element.nativeElement.value = ret;
+    }
+  }
+  private formataCampo(campo: string, Mascara: string, tamanho: number): any { 
+    var boleanoMascara; 
+    let exp = /\-|\.|\/|\(|\)| /g
+    let campoSoNumeros = campo.toString().replace( exp, "" ); 
+    var posicaoCampo = 0;    
+    var NovoValorCampo="";
+    var TamanhoMascara = campoSoNumeros.length;; 
+    for(let i=0; i< TamanhoMascara; i++) { 
+      if(i < tamanho) {
+        boleanoMascara  = ((Mascara.charAt(i) == "-") || (Mascara.charAt(i) == ".") || (Mascara.charAt(i) == "/")); 
+        boleanoMascara  = boleanoMascara || ((Mascara.charAt(i) == "(") || (Mascara.charAt(i) == ")") || (Mascara.charAt(i) == " "));
+        if (boleanoMascara) { 
+          NovoValorCampo += Mascara.charAt(i); 
+          TamanhoMascara++;
+        } else { 
+          NovoValorCampo += campoSoNumeros.charAt(posicaoCampo); 
+          posicaoCampo++; 
+        }
+      } 
+    }      
+    return NovoValorCampo; 
+  }
+
+}
+
